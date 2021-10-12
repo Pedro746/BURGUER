98a21ea33ae0a025ea0007da28cfca26cc8f0aaa
Pedro746 <pedrolima4680@gmail.com>
2021-10-12 12:33:30 -0300

    Cadastro de pedidos e gerenciamento de pedidos

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="main-banner">
+    <h1>Make Your Burguer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+};
+</script>
+
+<style scoped>
+#main-banner {
+  background-image: url("/img/burger.jpg");
+  background-repeat: no-repeat;
+  background-position: 0 -250px;
+  background-size: cover;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+#main-banner h1 {
+  color: #fff;
+  text-align: center;
+  font-size: 60px;
+  background: rgb(39, 39, 39, 0.8);
+  padding: 20px 40px;
+  border-left: 4px solid #fcba03;
+}
+</style>
